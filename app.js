02bf01e8bd73e7e0a56761aadf16551ba125eaa5
Coder Shiyar <codershiyar@gmail.com>
2023-02-23 21:06:33 +0100

    Fixed bugs, now Ai player is playing very well

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let Interval;
 let StandSpeler1=4;
 let StandSpeler2=4;
 
+
 function VeldenDeselecteren(ClassNaam){
     const AllToegestaanVelden = document.querySelectorAll('.'+ClassNaam);
     AllToegestaanVelden.forEach((element) => {
@@ -16,214 +17,166 @@ function VeldenDeselecteren(ClassNaam){
     });
 }
 
-function Bewegen(){
+function KlikOpEigenVak(CoordinateX,CoordinateY,Id){
     
+    VeldenDeselecteren("ToegestaanVelden1")
+    VeldenDeselecteren("ToegestaanVelden2")
+    localStorage.setItem("CoordinateX",CoordinateX)
+    localStorage.setItem("CoordinateY",CoordinateY-1 )
+
+    let ToegestaanStappen1 = [Id-1,Id+10,Id+1,Id-10, Id-9,Id-11, Id+9,Id+11]
+    ToegestaanStappen1.forEach(VeldId => {
+        if(VeldIdes.includes(VeldId)){
+            document.getElementById(VeldId).classList.add("ToegestaanVelden1")
+        }
+    });
+
+    let ToegestaanStappen2 = [Id-2,Id+2,Id+8,Id+12,Id+20,Id+19,Id+18,Id+21,Id+22,Id-20,Id-19,Id-18,Id-21,Id-22,Id-8,Id-12]
+    ToegestaanStappen2.forEach(VeldId => {
+        if(VeldIdes.includes(VeldId)){
+            document.getElementById(VeldId).classList.add("ToegestaanVelden2")
+        }
+    });
 }
+
+function KlikOpVeldenNietVanSpelers (CoordinateX,CoordinateY,Id){
+    let Beurt = Number(document.getElementById("Beurt").innerText);
+    console.log("2 Gaat naar " + Id)
+    let ToegestaanVelden = document.querySelectorAll(".ToegestaanVelden1, .ToegestaanVelden2");
+    if(ToegestaanVelden.length>0){
+     let KanBewegen = false;
+       for (let i = 0; i<ToegestaanVelden.length; i++) {
+         try {
+            if(Id ==ToegestaanVelden[i].id ){
+                KanBewegen = true;
+                OpslaanVeldInStapel(VeldMap)
+                VeldMap[CoordinateY-1][CoordinateX] = Beurt;
+                let NieuweId =""+ CoordinateY+ CoordinateX;
+                NieuweId = Number(NieuweId)
+                if(ToegestaanVelden[i].classList.contains("ToegestaanVelden2")){
+                    VeldMap[localStorage.getItem("CoordinateY")][localStorage.getItem("CoordinateX")] = 0;
+                }
+                BesmettenVijand(NieuweId)
+                Beurt = Beurt===2?1:2;
+                document.getElementById("Beurt").innerText =Beurt;
+                if(Beurt==2){ setTimeout(() => { StartTegenSpeler2()}, 300); }
+            }
+        } catch (error) {
+            // Beurt = Beurt===2?1:2;
+            try {
+                document.getElementById("Beurt").innerText =Beurt;
+            } catch (error) { 
+                console.log("Speel is beindiged")
+            }
+            if(Beurt==2){ setTimeout(() => { StartTegenSpeler2()}, 300); }
+        }   
+       }
+       if(!KanBewegen){
+            VeldenDeselecteren("ToegestaanVelden1"); VeldenDeselecteren("ToegestaanVelden2")
+       }
+    } 
+}
+
+function BesmettenVijand(Id){
+    let Beurt = Number(document.getElementById("Beurt").innerText);
+    let ToegestaanStappen1 = [Id-1,Id+10,Id+1,Id-10, Id-9,Id-11, Id+9,Id+11]        
+    for (let index = 0; index < ToegestaanStappen1.length; index++) {
+            var digits = ToegestaanStappen1[index].toString().split('').map(Number);
+            let Tegenstanderd = Beurt==2?1:2;
+            if(Tegenstanderd==Number(VeldMap[digits[0]-1][digits[1]])){
+                VeldMap[digits[0]-1][digits[1]] = Beurt;
+            }
+    }
+    VeldAanMaken(VeldMap)
+}
+
 function HandelVeldklik(GeselecteerdVakIsVan, CoordinateX,CoordinateY,Id){
     let Beurt = Number(document.getElementById("Beurt").innerText);
     if (GeselecteerdVakIsVan>0){
-        
         if (GeselecteerdVakIsVan === Beurt){
-            VeldenDeselecteren("ToegestaanVelden1")
-            VeldenDeselecteren("ToegestaanVelden2")
-            localStorage.setItem("CoordinateX",CoordinateX)
-            localStorage.setItem("CoordinateY",CoordinateY-1 )
-
-            let ToegestaanStappen1 = [Id-1,Id+10,Id+1,Id-10, Id-9,Id-11, Id+9,Id+11]
-            ToegestaanStappen1.forEach(VeldId => {
-                if(VeldIdes.includes(VeldId)){
-                    document.getElementById(VeldId).classList.add("ToegestaanVelden1")
-                }
-            });
-
-            let ToegestaanStappen2 = [Id-2,Id+2,Id+8,Id+12,Id+20,Id+19,Id+18,Id+21,Id+22,Id-20,Id-19,Id-18,Id-21,Id-22,Id-8,Id-12]
-            ToegestaanStappen2.forEach(VeldId => {
-                if(VeldIdes.includes(VeldId)){
-                    document.getElementById(VeldId).classList.add("ToegestaanVelden2")
-                }
-            });
-     
+            KlikOpEigenVak(CoordinateX,CoordinateY,Id);
         }else{
-            if(Beurt ==2){
-                Speler2()
-            }
             document.getElementById("Alert").innerHTML = "<span class='NietJeBeurt'> Het is niet je Beurt </span> <br> Het is Beurt speler <span id='Beurt'>" +Beurt + "</span>"
         }
     }else{
-
-        let ToegestaanVelden1 = document.getElementsByClassName("ToegestaanVelden1").length
-        let ToegestaanVelden2 = document.getElementsByClassName("ToegestaanVelden2").length;
-        
-        if(ToegestaanVelden1>0 || ToegestaanVelden1>2){
-
-            let KanBewegen = false;
-           for (let i = 0; i<ToegestaanVelden1; i++) {
-   
-             try {
-    
-                if(Id ==document.getElementsByClassName("ToegestaanVelden1")[i].id ){
-                    KanBewegen = true;
-                    OpslaanVeldInStapel(VeldMap)
-                    VeldMap[CoordinateY-1][CoordinateX] = Beurt;
-                    let NieuweId =""+ CoordinateY+ CoordinateX;
-                    NieuweId = Number(NieuweId)
-                    let ToegestaanStappen1 = [NieuweId-1,NieuweId+10,NieuweId+1,NieuweId-10, NieuweId-9,NieuweId-11, NieuweId+9,NieuweId+11]
-                   
-                    for (let index = 0; index < ToegestaanStappen1.length; index++) {
-                        try {
-                            var digits = ToegestaanStappen1[index].toString().split('').map(Number);
-                            let Tegenstanderd = Beurt==2?1:2;
-                            if(Tegenstanderd==Number(VeldMap[digits[0]-1][digits[1]])){
-                                VeldMap[digits[0]-1][digits[1]] = Beurt;
-                            }
-                        } catch (error) {
-                            console.log(error)
-                        }
-                      
-                    }
-
-                    VeldAanMaken(VeldMap)
-                    Beurt = Beurt===2?1:2;
-                    document.getElementById("Beurt").innerText =Beurt
-                    if(Beurt==2){
-                       setTimeout(() => { Speler2()}, 500);
-                    }
-                }
-            } catch (error) {}
-               
-           }
-
-
-           for (let i = 0; i<ToegestaanVelden2; i++) {
-
-            try {
-           
-               if(Id == document.getElementsByClassName("ToegestaanVelden2")[i].id ){
-                console.log("Punt 6 Aanval door" + Beurt, Id)
-                   KanBewegen = true;
-                   OpslaanVeldInStapel(VeldMap)
-                   VeldMap[CoordinateY-1][CoordinateX] = Beurt;
-                   VeldMap[localStorage.getItem("CoordinateY")][localStorage.getItem("CoordinateX")] = 0;
-                   Beurt = Beurt===2?1:2;
-                   VeldAanMaken(VeldMap)
-                   document.getElementById("Beurt").innerText =Beurt
-               }
-           } catch (error) { 
-            console.log("Punt 7 Aanval door" + Beurt, Id)
-            console.log(error)
-            console.log(document.getElementsByClassName("ToegestaanVelden2"))
-        }
-              
-          }
-
-           if(!KanBewegen){
-            for (let i2 = 0; i2<ToegestaanVelden1; i2++) {
-                VeldenDeselecteren("ToegestaanVelden1")
-                VeldenDeselecteren("ToegestaanVelden2")
-            }
-           }else{
-            
-           }
-        } 
+        KlikOpVeldenNietVanSpelers (CoordinateX,CoordinateY,Id); 
     }
-
 }
 
-function Speler2(){
+
+function StartTegenSpeler2(){
     let VeldenVanSpeler2 = document.getElementsByClassName("Speler2"); 
     var MogelijkeBesmettingenPerElement = []  
     for (let index = 0; index < VeldenVanSpeler2.length; index++) {
         const element = VeldenVanSpeler2[index];
         let Id = element.id;
-        let ToegestaanStappen = [Id-1,Id+10,Id+1,Id-10, Id-9,Id-11, Id+9,Id+11,Id-2,Id+2,Id+8,Id+12,Id+20,Id+19,Id+18,Id+21,Id+22,Id-20,Id-19,Id-18,Id-21,Id-22,Id-8,Id-12]
+        let ToegestaanStappen = [Id-1,Id+10,Id+1,Id-10, Id-9,Id-11,Id+9,Id+11,Id-2,Id+2,Id+8,Id+12,Id+20,Id+19,Id+18,Id+21,Id+22,Id-20,Id-19,Id-18,Id-21,Id-22,Id-8,Id-12]
         let MogelijkeBesmettingen = 0;
         ToegestaanStappen.forEach(VeldId => {
             if(VeldIdes.includes(VeldId)){
-                if( document.getElementById(VeldId).classList.contains("Speler1")){
-                    MogelijkeBesmettingen++;
+                if(document.getElementById(VeldId).classList.contains("Speler1")){
+                   ++MogelijkeBesmettingen;
                 }
             }
         });
         MogelijkeBesmettingenPerElement.push([Id,MogelijkeBesmettingen])
     }
-    let Gevonden = false;
-    MogelijkeBesmettingenPerElement.forEach(Element=>{
+    KlikOpDeBestVeldVanSpeler2(MogelijkeBesmettingenPerElement)
+}
+
+function KlikOpDeBestVeldVanSpeler2(MogelijkeBesmettingenPerElement){
+let Gevonden = false;
+ MogelijkeBesmettingenPerElement.forEach(Element=>{
         if ((!Gevonden) && Element[1] ===  Math.max(...MogelijkeBesmettingenPerElement.map(o => o[1]))) {
             Gevonden=true;
             document.getElementById(Element[0]).click();
-            setTimeout(() => { AanvalenSpeler1();  }, 500);
+            setTimeout(() => { CheckBestVakVoorAanval();  }, 300);
         }
     })
 
-    
-}
-function KiesdebesteUitOptie(){
-    const AllToegestaanVelden = document.querySelectorAll('.ToegestaanVelden1,.ToegestaanVelden2 ');
-    let MogelijkeBesmettingenPerElement = []  
-    for (let index = 0; index < AllToegestaanVelden.length; index++) {
-        const element = AllToegestaanVelden[index];
-        let Id = element.id;
-        let ToegestaanStappen = [Id-1,Id+10,Id+1,Id-10, Id-9,Id-11, Id+9,Id+11,Id-2,Id+2,Id+8,Id+12,Id+20,Id+19,Id+18,Id+21,Id+22,Id-20,Id-19,Id-18,Id-21,Id-22,Id-8,Id-12]
-        let MogelijkeBesmettingen = 0;
-        ToegestaanStappen.forEach(VeldId => {
-            if(VeldIdes.includes(VeldId)){
-                if( document.getElementById(VeldId).classList.contains("Speler1")){
-                    MogelijkeBesmettingen++;
-                }
-            }
-        });
-        MogelijkeBesmettingenPerElement.push([Id,MogelijkeBesmettingen])
-       
-    }
-}
- 
-function AanvalenSpeler1(){
-    const AllToegestaanVelden = document.querySelectorAll('.ToegestaanVelden1,.ToegestaanVelden2 ');
-    let MogelijkeBesmettingenPerElement = []  
-    for (let index = 0; index < AllToegestaanVelden.length; index++) {
-        const element = AllToegestaanVelden[index];
-        let Id = element.id;
-        let ToegestaanStappen = [Id-1,Id+10,Id+1,Id-10, Id-9,Id-11, Id+9,Id+11,Id-2,Id+2,Id+8,Id+12,Id+20,Id+19,Id+18,Id+21,Id+22,Id-20,Id-19,Id-18,Id-21,Id-22,Id-8,Id-12]
-        let MogelijkeBesmettingen = 0;
-        ToegestaanStappen.forEach(VeldId => {
-            if(VeldIdes.includes(VeldId)){
-                if( document.getElementById(VeldId).classList.contains("Speler1") ){
-                    MogelijkeBesmettingen++;
-                }
-            }
-        });
-        MogelijkeBesmettingenPerElement.push([Id,MogelijkeBesmettingen])
-      
-    }
-    console.log(MogelijkeBesmettingenPerElement)
-    // console.clear()
-    let Gevonden = false;
-    while(!Gevonden){
-
-        let MaxMogelijkeBesmettingen = Math.max(...MogelijkeBesmettingenPerElement.map(o => o[1]))
-        MogelijkeBesmettingenPerElement.forEach(Element=>{
-            if (Element[1] ===  MaxMogelijkeBesmettingen) {
-                if (!Gevonden) {
-                    console.log(Element)
-                    if((!document.getElementById(""+Element[0]).classList.contains("Speler1")) && (!document.getElementById(""+Element[0]).classList.contains("Speler2"))) {
-                        console.log("Kan",Element)
-                        if (!document.getElementById(Element[0]).classList.contains("Speler")) {
-                            document.getElementById(Element[0]).click();
-                            Gevonden =true;
-                        }
-                        
-                    }else{
-                        console.log("Kan niet, er is een speler1",Element)
-                        MogelijkeBesmettingenPerElement = MogelijkeBesmettingenPerElement.filter(function(e) { return e !== Element })
-                    }  
-                }
-            }
-        })
-  
-    }
-    
 }
 
+function CheckBestVakVoorAanval(){
+    let ToegestaanVelden = document.querySelectorAll('.ToegestaanVelden1,.ToegestaanVelden2');
+   
+    let MogelijkeBesmettingenPerVak = []  
+    for (let index = 0; index < ToegestaanVelden.length; index++) {
+        let Id = ToegestaanVelden[index].id;
+    
+        if(!ToegestaanVelden[index].classList[0].includes("Speler")){
+            let ToegestaanStappen = [Id-1,Id+10,Id+1,Id-10, Id-9,Id-11, Id+9,Id+11]
+            let MogelijkeBesmettingen = 0;
+            ToegestaanStappen.forEach(VeldId => {
+                if(VeldIdes.includes(VeldId)){
+                    if(document.getElementById(VeldId).classList.contains("Speler1") ){
+                        MogelijkeBesmettingen++;
+                    }
+                }
+            });
+            MogelijkeBesmettingenPerVak.push([Id,MogelijkeBesmettingen])
+        }
+         }
+         BeginAiAanval(MogelijkeBesmettingenPerVak)
+}
+
+function BeginAiAanval(MogelijkeBesmettingenPerVak){
+
+        let MaxMogelijkeBesmettingen = Math.max(...MogelijkeBesmettingenPerVak.map(o => o[1]))
+        let Gevonden = false;
+        if(MaxMogelijkeBesmettingen ==0 && MogelijkeBesmettingenPerVak.length>0){
+            document.getElementById(MogelijkeBesmettingenPerVak[1][0]).click();
+        }else{
+            MogelijkeBesmettingenPerVak.forEach(Element=>{
+                if (Element[1] ===  MaxMogelijkeBesmettingen) {
+                    if (!Gevonden) {
+                        console.log("Gaat naar " + Element[0])
+                        document.getElementById(Number(Element[0])).click();
+                        Gevonden =true;
+                    }
+                }
+            })
+        }
+}
 let SpeelBegonnenKnop = document.getElementById("SpeelBegonnenKnop");
 SpeelBegonnenKnop.onclick = () =>{
     SpeelIsBegonnen=!SpeelIsBegonnen;
@@ -239,6 +192,8 @@ SpeelBegonnenKnop.onclick = () =>{
 }
   
 }
+
+
 
 function Startopstelling() {
     SpeelBegonnenKnop.innerText = "Eind speel"
@@ -269,7 +224,6 @@ function NieuweMapAanmaken(){
                 VeldRij.length === 0 || VeldRij.length === 1 ? VeldRij.push(2): VeldRij.push(0)
             else
                 VeldRij.push(0)
-        
         }
         Veld.push(VeldRij)
     }
@@ -293,13 +247,12 @@ function OpslaanVeldInStapel(Waarde){
           body:JSON.stringify({"Waarde": JSON.stringify(Waarde)}) 
     }).then(Request=> Request.text()).then(Antwoord=>{
         // console.log("Gestuurd: "+ Antwoord)
+    }).catch(error=>{
+        console.log("Catch error", error)
     })
 }
 
-
 function VeldAanMaken(Veld){
-
-   
     if(StandSpeler1==0 || StandSpeler2==0){
        SpeelAfgelopen()
     }
@@ -344,13 +297,10 @@ function VeldAanMaken(Veld){
     UpdateStand()
 }
 
-
-
 function VerhogenVeldGrootte(){
    VeldGrootte.innerText = Number(VeldGrootte.innerText) + 1;
    UpdateLayout()
    VeldAanMaken(NieuweMapAanmaken())
- 
 }
 function VerlagenVeldGrootte(){
     VeldGrootte.innerText>6?   VeldGrootte.innerText = Number(VeldGrootte.innerText) -1:""
